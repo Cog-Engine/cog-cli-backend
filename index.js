fcbbaa8186a0e982fcb7bb09d4cf06b5c12bddb4
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2023-12-14 20:05:42 +0800

    Fixing typescript setup, modify package.json

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Example = void 0;
+function Example() {
+    console.log("Call successfully");
+    return true;
+}
+exports.Example = Example;
